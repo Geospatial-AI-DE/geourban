0978a4014b64cb5857c88bea669ed7b76ddb6f8d
gisfromscratch <gisfromscratch@live.de>
2024-01-07 18:52:38 +0100

    Added a docstring for the types

--- geourban/types.py
+++ geourban/types.py
@@ -12,6 +12,23 @@ class GridType(Enum):
     agent: The number of unique agents is calculated.
     speed: The speed average of every agent is calculated.
     emissions: The sum of carbon dioxide emissions of every agent is calculated. This makes only sense for vehicle being cars!
+
+    Example:
+
+    .. code-block:: python
+    
+        from geourban.types import GridType
+
+        # Returns traffic grid cells accumulating the number of unique agents
+        grid_type = GridType.AGENT
+
+        # Returns traffic grid cells accumulating the speed average or every agent
+        grid_type = GridType.SPEED
+
+        # Returns traffic grid cells accumulating the sum of carbon dioxide emissions (kg/km) of every agent
+        # This makes only sense for vehicle being cars!
+        grid_type = GridType.EMISSIONS
+
     """
     AGENT=0
     SPEED=1
@@ -32,6 +49,22 @@ class VehicleType(Enum):
     """
     Represents the supported vehicle types.
     Car, Bike and Pedestrian are possible vehicle types.
+
+    Example:
+
+    .. code-block:: python
+    
+        from geourban.types import VehicleType
+
+        # Returns traffic grid cells from agents moving by car
+        vehicle_type = VehicleType.CAR
+
+        # Returns traffic grid cells from agents moving by bike
+        vehicle_type = VehicleType.BIKE
+
+        # Returns traffic grid cells from agents walking
+        vehicle_type = VehicleType.PEDESTRIAN
+
     """
     CAR=0
     BIKE=1
